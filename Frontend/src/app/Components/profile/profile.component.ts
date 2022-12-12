3f6960165539b588446374e93dd9c9d56df298b4
ntokozo-1998 <94315784+ntokozo-1998@users.noreply.github.com>
2022-12-12 09:11:58 +0200

    form done

--- Frontend/src/app/Components/profile/profile.component.ts
+++ Frontend/src/app/Components/profile/profile.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/service/auth.service';
+import { JwtService } from 'src/app/service/jwt.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +15,158 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+
+  updateForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    name: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
+    surname: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
+    confirmPassword: new FormControl(),
+    //account: new FormControl()
+  });
+
+  username ?:string;
+  name ?:string;
+  surname ?:string;
+  email ?:string;
+  account ?:string;
+  user_id ?:string;
+  image_link:any='';
+  rating : number = 0;
+  showRating :boolean = true;
+
+  preset :string = "i8maua2c";
+
+  update_dp = new FormGroup({
+    file:new FormControl(),
+    upload_preset: new FormControl()}
+  );
+
+  cloudinaryUrl: string = 'https://api.cloudinary.com/v1_1/dev-lab/image/upload';
+  file: any;
+  isUpdating: boolean = false;
+
+
+  constructor(private userService:UserService,private authService: AuthService, private router: Router,private toastr: ToastrService ,private jwt:JwtService,private spinner:NgxSpinnerService, private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.spinner.show();
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 1000);
+
+    this.username = this.jwt.getData(JSON.stringify(localStorage.getItem('token'))).username; 
+    this.surname = this.jwt.getData(JSON.stringify(localStorage.getItem('token'))).surname;
+    this.name = this.jwt.getData(JSON.stringify(localStorage.getItem('token'))).name;
+    this.email = this.jwt.getData(JSON.stringify(localStorage.getItem('token'))).email;
+    this.account = this.jwt.getData(JSON.stringify(localStorage.getItem('token'))).account;
+    this.user_id = this.jwt.getData(JSON.stringify(localStorage.getItem('token'))).user_id;
+
+
+    if(this.account =='Client')
+    {
+      this.showRating = false;
+    }else
+    {
+      this.showRating = true;
+
+    }
+
+
+
+    this.userService.getOneUser(this.user_id).subscribe((data:any)=>{
+      this.rating = data.rating;
+
+    })
+    
+    this.image_link = localStorage.getItem('image_link')
+
+    
+
   }
+
+  toggleUpdate() {
+    if(this.isUpdating)
+    {
+      this.isUpdating = false;
+    }else{
+      this.isUpdating = true;
+    }
+
+  }
+
+
+  onUpdate(form: FormGroup)
+  {
+    if(form.valid){
+      if(form.value.confirmPassword == form.value.password)
+      {
+        this.userService.updateProfile(this.user_id, form.value).subscribe((data:any)=>{
+          this.toastr.success(data.message);
+          if(this.account == 'Freelancer')
+          {
+            this.router.navigateByUrl('/freelancer');
+          }else
+          {
+            this.router.navigateByUrl('/client');
+    
+          }
+        },(err:HttpErrorResponse)=>{
+          this.toastr.error(err.error.message);
+          
+        })
+
+      }else{
+        this.toastr.warning('Passwords do not match');
+      }
+
+    }
+    
+    
+  }
+
+  async onFileChange(event :any)
+  {
+    this.spinner.show();
+
+    if(event.target.files.length>0)
+    {
+      this.file =  event.target.files[0];
+      this.spinner.hide();
+     
+    }
+
+  }
+
+  image = {
+    link : '' 
+  }
+
+
+  onSubmit(){  
+    
+    this.spinner.show();
+    const formData = new FormData();    
+    formData.append("file",this.file)    
+    formData.append("upload_preset","i8maua2c");     
+    this.http.post(this.cloudinaryUrl,formData).subscribe(async (res:any)=>{     
+      this.image_link = await res.url;
+      this.image.link = this.image_link;
+      // console.log(res)
+      //this.userService.updateProfilePicture(this.user_id,this.image).subscribe((saveData:any)=>{
+        this.toastr.success("Image updated successfully");
+        localStorage.setItem('image_link',this.image_link);
+
+        this.spinner.hide();
+        //console.log(saveData);
+      })
+      //console.log(this.image_link);
+    //}) 
+    
+    
+   
+  }
+  
 
 }
--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import * as path from 'path';
 import { DoneeComponent } from './Components/donee/donee.component';
 import { DonorComponent } from './Components/donor/donor.component';
+import { FormComponent } from './Components/form/form.component';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { ProfileComponent } from './Components/profile/profile.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { TrackComponent } from './Components/track/track.component';
+
 
 const routes: Routes = [
 
@@ -20,6 +22,8 @@ const routes: Routes = [
   {path : 'home' , component : HomeComponent},
   {path : 'profile' , component : ProfileComponent},
   {path : 'track' , component : TrackComponent},
+  {path : 'form' , component : FormComponent},
+  
 ]
 
 
